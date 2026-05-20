d81357329d100f74c1fb0397c877709a35bfe5f1
Andreas Wolf <powerflow11@web.de>
2026-05-20 02:47:44 +0200

    fix: regenerate lockfile via Noesis strategy

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,6 +1,7 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-// three.js and expo-three need .glsl/.vs/.fs support optional; we don't use external shaders
+// three.js und expo-three brauchen keine extra Shader-Konfiguration,
+// daher exportieren wir einfach die Standard-Konfiguration.
 module.exports = config;
